d73afcbd699cc4e4ba30c32b0a80de6fb58bbc10
qkrwns1478 <qkrwns1478@gmail.com>
2025-07-01 13:36:09 +0900

    exp: 버튼 테스트 API 구현

--- backend/index.js
+++ backend/index.js
@@ -63,6 +63,52 @@ app.post('/api/analytics/collect', async (req, res) => {
   }
 });
 
+app.get('/api/button-clicks', async (req, res) => {
+  // const data = req.body;
+  try {
+    const db = await connectMongo();
+    const logs = db.collection('logs');
+
+    const queries = await logs
+      .find({
+        $and: [
+          { event_name: "auto_click" },
+          { "properties.target_text": /^Button [1-7]$/ },
+          { "properties.is_button": true }
+        ]
+      })
+      .toArray();
+
+    let clicks = [0, 0, 0, 0, 0, 0, 0, 0];
+    for (let i = 0; i < queries.length; i++) {
+      const tmp = queries[i].properties.target_text;
+      clicks[Number(tmp.charAt(tmp.length - 1))]++;
+    }
+
+    const buttonClicks = Object.fromEntries(
+      Array.from({ length: 7 }, (_, i) => {
+        const index = i + 1; // 1부터 시작
+        return [`button${index}`, clicks[index]];
+      })
+    );
+
+    const clickEvents = Object.fromEntries(
+      Array.from({length: 7}, (_, i) => {
+        const index = i + 1;
+        return {
+          element_path: `button:nth-child(${index})`,
+          target_text: `Button ${index}`,
+        };
+      })
+    );
+
+    res.status(200).json({ buttonClicks: buttonClicks, clickEvents: clickEvents });
+  } catch (err) {
+    console.error('MongoDB FIND ERROR:', err);
+    res.status(500).json({ error: 'MongoDB find failed' });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`KlickLab 서버 실행 중: http://localhost:${PORT}`);
 });
